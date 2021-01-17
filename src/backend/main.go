daddc3dcea7569133831c143a6f7cd825850e7de
Babaranti Familusi <ranti.familusi@nttdata.com>
2021-01-16 23:15:58 -0700

    add more methods

--- src/backend/main.go
+++ src/backend/main.go
@@ -5,9 +5,12 @@ import (
 	"log"
 	"encoding/json"
 	"net/http"
+	"github.com/gorilla/mux"
+	"io/ioutil"
 )
 
 type Article struct {
+	Id      string `json:"Id"`
     Title string `json:"Title"`
     Desc string `json:"desc"`
     Content string `json:"content"`
@@ -27,17 +30,74 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request){
     json.NewEncoder(w).Encode(Articles)
 }
 
+func returnSingleArticle(w http.ResponseWriter, r *http.Request){
+    vars := mux.Vars(r)
+    key := vars["id"]
+
+	
+	
+	// Loop over all of our Articles
+    // if the article.Id equals the key we pass in
+    // return the article encoded as JSON
+    for _, article := range Articles {
+        if article.Id == key {
+            json.NewEncoder(w).Encode(article)
+        }
+    }
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+    // get the body of our POST request
+    // return the string response containing the request body    
+    reqBody, _ := ioutil.ReadAll(r.Body)
+    var article Article 
+    json.Unmarshal(reqBody, &article)
+    // update our global Articles array to include
+    // our new Article
+    Articles = append(Articles, article)
+
+    json.NewEncoder(w).Encode(article)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+    // once again, we will need to parse the path parameters
+    vars := mux.Vars(r)
+    // we will need to extract the `id` of the article we
+    // wish to delete
+    id := vars["id"]
+
+    // we then need to loop through all our articles
+    for index, article := range Articles {
+        // if our id path parameter matches one of our
+        // articles
+        if article.Id == id {
+            // updates our Articles array to remove the 
+            // article
+            Articles = append(Articles[:index], Articles[index+1:]...)
+        }
+    }
+
+}
+
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", returnAllArticles)
-	log.Fatal(http.ListenAndServe(":8088", nil))
+
+	// creates a new instance of a mux router
+    myRouter := mux.NewRouter().StrictSlash(true)
+	  // replace http.HandleFunc with myRouter.HandleFunc
+	  myRouter.HandleFunc("/", homePage)
+	  myRouter.HandleFunc("/articles", returnAllArticles)
+	  myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	  // add our new DELETE endpoint here
+	  myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	  myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	log.Fatal(http.ListenAndServe(":8088", myRouter))
 }
 
 func main() {
-
+	fmt.Println("Rest API v2.0 - Mux Routers")
 	Articles = []Article{
-        Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-        Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-	}
+        Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+        Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+    }
 	handleRequests()
 }
